5afa35b7fb6f3c5c95525011c07af531a1c4bb9d
d3miurg <50408380+d3miurg@users.noreply.github.com>
2024-03-05 19:18:20 +0300

    added trimming function

--- Minlang/parser.cpp
+++ Minlang/parser.cpp
@@ -3,12 +3,19 @@
 
 using namespace std;
 
+string trim_command(string command, int command_lenght)
+{
+	command.erase(0, command_lenght);
+	command.erase(command.length() - 1, command.length());
+	return command;
+}
+
 void parse_command(string *command_line, map<string, unsigned> memory) 
 {	
-	string value = *command_line;
-	cout << value << endl;
-	if (value.starts_with("new_name"))
+	string command = *command_line;
+	if (command.starts_with("new_name"))
 	{
-		cout << "MATCH" << endl;
+		command = trim_command(command, 9);
+		cout << command << endl;
 	}
 }
